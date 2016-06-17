18ea97bfe240245931ed01a0f0930efc0c1c8f4d
wandersoncs <wandersoncorreiasouza@gmail.com>
2016-06-16 23:31:24 -0300

    identacao e AutenticacaoFilter alterado

--- src/br/ufc/qx/tizeeter/controller/AutenticarUsuarioController.java
+++ src/br/ufc/qx/tizeeter/controller/AutenticarUsuarioController.java
@@ -17,29 +17,29 @@ public class AutenticarUsuarioController extends TizeeterGenericBaseHttpServlet<
 
 	private static final String FLAG_AUTENTIFICADO = "autenticado";
 	private static final long serialVersionUID = 1979208128920557545L;
-	
+
 	public AutenticarUsuarioController() {
 		setDAO(new UsuarioDAO());
 	}
-	
+
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
 		UsuarioDAO usuarioDAO = getDAO();
 		usuarioDAO.setConexao((Connection) request.getAttribute("connection"));
 		boolean autenticado = usuarioDAO.autenticar(login, senha);
-		
-		if(autenticado){
+
+		if (autenticado) {
 			HttpSession sessao = request.getSession(true);
 			sessao.setAttribute(FLAG_AUTENTIFICADO, true);
-			
+
 			Usuario usuario = usuarioDAO.getUsuario(login);
 			sessao.setAttribute("usuario", usuario);
 			sessao.setAttribute("admin", usuario.isAdmin());
 			response.sendRedirect("home");
-		} else {			
+		} else {
 			request.setAttribute(FLAG_AUTENTIFICADO, false);
 			request.getRequestDispatcher("login.jsp").forward(request, response);
 		}
--- src/br/ufc/qx/tizeeter/filter/AutenticacaoFilter.java
+++ src/br/ufc/qx/tizeeter/filter/AutenticacaoFilter.java
@@ -22,15 +22,17 @@ public class AutenticacaoFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
+
 		String URI = ((HttpServletRequest) request).getRequestURI();
 		HttpSession session = ((HttpServletRequest) request).getSession();
+
 		if (URI.equals("/Tizeeter/"))
 			((HttpServletResponse) response).sendRedirect("home");
 		else if (URI.contains("login.jsp") || URI.contains("autenticar") || URI.contains("cadastrar.jsp")
 				|| URI.endsWith(".css") || URI.endsWith(".js") || URI.endsWith(".ico") || URI.endsWith(".png")
 				|| URI.contains("403.jsp") || URI.contains("usuario/novo") || URI.contains("logout"))
 			chain.doFilter(request, response);
-		else if (session.getAttribute("autenticado") != null) {
+		else if (session.getAttribute("autenticado") != null && (boolean) session.getAttribute("autenticado")) {
 			if ((URI.contains("todos") && (boolean) session.getAttribute("admin")) || URI.contains("home")
 					|| URI.contains("index.jsp") || URI.contains("tizeet"))
 				chain.doFilter(request, response);
